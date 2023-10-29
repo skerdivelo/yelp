6bd1805a53431b0dd88c9e7ebc907ebdfdd9b565
Skerdi Velo <skerdivelo42@gmail.com>
2023-10-29 16:35:28 +0100

    Some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RestaurantdetailPage from "./routes/RestaurantdetailPage";
+import UpdatePage from "./routes/UpdatePage";
+import Home from "./routes/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route
+                        path="/restaurants/:id/update"
+                        element={<UpdatePage />}
+                    />
+                    <Route
+                        path="/restaurants/:id"
+                        element={<RestaurantdetailPage />}
+                    />
+                </Routes>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/routes/RestaurantdetailPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RestaurantdetailPage = () => {
+    return <div>Detail</div>;
+};
+
+export default RestaurantdetailPage;
